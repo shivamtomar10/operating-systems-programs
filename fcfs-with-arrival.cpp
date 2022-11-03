24f10995f52aac277848cdf62e069b1a2c329733
@shivam <98133568+shivamtomar10@users.noreply.github.com>
2022-11-03 23:19:38 +0530

    Update fcfs-with-arrival.cpp

--- fcfs-with-arrival.cpp
+++ fcfs-with-arrival.cpp
@@ -1,77 +1,60 @@
 #include<iostream>
-using namespace std;
 #include<algorithm>
+using namespace std;
 
+// fcfs
 int main(){
     
-    cout<<"Enter the number of process you want:";
-    int pr;
-    cin>>pr;
+    cout<<"Enter the process:";
+    int process,p=1;
+    cin>>process;
     
-    int arr[pr],bur[pr],comp[pr],temp[pr],p=1,temp2[pr];
+    int arrival[process],burst[process],comp[process],wat[process],tat[process],temp[process],temp1[process];
     
-    for(int i=0;i<pr;i++){
-        cout<<"Enter the arrival of "<<p++<<" process is:";
-        cin>>arr[i];
-        temp[i]=arr[i];
+    for(int i=0;i<process;i++){
+        cout<<"Enter the arrival time of "<<p++<<":";
+        cin>>arrival[i];
+        temp[i]=arrival[i];
     }
+    
+    sort(temp,temp+process);
     p=1;
-    for(int i=0;i<pr;i++){
-        cout<<"Enter the burst time of "<<p++<<" process is:";
-        cin>>bur[i];
+    for(int i=0;i<process;i++){
+        cout<<"Enter the burst time of "<<p++<<":";
+        cin>>burst[i];
     }
     
-    sort(temp,temp+pr);
-    
-    int arr1[pr],l=0,f=0,value=0,h=0,t=0;
-    for(int i=0;i<pr;i++){
-        if(i==0){
-            value=temp[i];
-        }
-        else{
-            i+=h-1;
-            value=temp[i];
-        }
-        cout<<"Value is:"<<value<<endl;
-        int steps=0;
-        h=0;
-        for(int k=0;k<pr;k++){
-            if(value==arr[k]){
-                arr1[h++]=bur[steps];
+    for(int i=0;i<process;i++){
+        int value=temp[i],steps=0;
+        
+        for(int i=0;i<process;i++){
+            if(value==arrival[i]){
+                break;
             }
             steps++;
+                
         }
-        sort(arr1,arr1+h);
         
-        for(int h1=0;h1<h;h1++){
-            comp[l++]=arr1[h1];
+        comp[i]=burst[steps];
+        if(i!=0){
+            comp[i]+=comp[i-1];
         }
-        for(int g=0;g<h;g++){
-            temp2[f++]=comp[t++];
+        
+        if(i!=0 && temp[i]>comp[i-1]){
+            comp[i]+=temp[i]-comp[i-1];
         }
+        
+        temp1[i]=burst[steps];
     }
-    
-    for(int i=1;i<pr;i++){
-        comp[i]=comp[i]+comp[i-1];
-    }
-    int tat[pr],wat[pr];
-    int g1=1,g2=1,g3=1;
-    for(int i=0;i<pr;i++){
+    float sum=0,sum1=0;
+    for(int i=0;i<process;i++){
         tat[i]=comp[i]-temp[i];
-        wat[i]=tat[i]-temp2[i];
-        
-        cout<<"The comp time of "<<g2++<<" process is:"<<comp[i]<<endl;
-        cout<<"The tat of "<<g1++<<" process is :"<<tat[i]<<endl;
-        cout<<"The wat time of "<<g3++<<" process is:"<<wat[i]<<endl;
+        wat[i]=tat[i]-temp1[i];
+        sum+=tat[i];
+        sum1+=wat[i];
     }
+    cout<<"The average tat time is :"<<sum/process<<endl;
+    cout<<"The average wat time is :"<<sum1/process<<endl;
     
-    int ttat=0,twat=0;
-    for(int i=0;i<pr;i++){
-        ttat+=tat[i];
-        twat+=wat[i];
-    }
-    
-    cout<<"The average tat is :"<<(float)(ttat/pr)<<endl;
-    cout<<"The average wat is:"<<(float)(twat/pr)<<endl;
     return 0;
 }
